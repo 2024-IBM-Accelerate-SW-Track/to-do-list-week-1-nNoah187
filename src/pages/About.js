773735a179b8918efa3f356136eb90ef86d0c677
nNoah187 <noahrg17@gmail.com>
2024-06-07 18:56:02 -0500

    Finished assignment

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+import image from "../assets/image.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+ <div className="split left">
+ <div className="centered">
+ <img 
+ className="profile_image"
+ src={image}
+ alt="Profile Pic"
+ ></img>
+ </div>
+ </div>
+ <div className="split right">
+ <div className="centered">
+ <div className="name_title">Noah Gibson</div>
+ <div className="brief_description">
+  I work and Target and am developing a website for a startup in Oklahoma City in my free time. I really like Chipotle.
+ </div>
+ </div>
+ </div>
+ </div>  
       </div>
     )
   }
